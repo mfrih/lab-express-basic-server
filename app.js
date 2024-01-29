5c790d94ad20bdac9113d737a847ea51a5da2950
Maria Frih <maria.frih@gmail.com>
2024-01-29 15:10:54 +0100

    organizes files and creates express server

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 // IMPORT PACKAGES
 // Here you should import the required packages for your Express app: `express` and `morgan`
-
-
+const express = require("express");
+const morgan = require("morgan");
 
 // CREATE EXPRESS APP
 // Here you should create your Express app:
-
-
+const app = express();
 
 // MIDDLEWARE
 // Here you should set up the required middleware:
 // - `express.static()` to serve static files from the `public` folder
+app.use(express.static("public"));
 // - `express.json()` to parse incoming requests with JSON payloads
+app.use(express.json());
 // - `morgan` logger to log all incoming requests
-
-
+app.use(morgan("dev"));
 
 // ROUTES
 // Start defining your routes here:
 
-
-
 // START THE SERVER
 // Make your Express server listen on port 5005:
+app.listen(5005, () => {
+  console.log(`Server is ⛹🏽‍♀️ on http://localhost:5005`);
+});
